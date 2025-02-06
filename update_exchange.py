8c6362e56b99ecabce2127f532d8fdec1e33b8ba
roddms <kke3007@sookmyung.ac.kr>
2025-02-06 16:09:58 +0900

    EUR로 변경

--- update_exchange.py
+++ update_exchange.py
@@ -17,9 +17,9 @@ def get_exchange():
     response = requests.get(URL)
     if response.status_code == 200:
         data = response.json()
-        nm = next((item["cur_nm"] for item in data if item["cur_unit"] == "KRW"), None)
-        ttb = next((item["ttb"] for item in data if item["cur_unit"] == "KRW"), None)
-        tts = next((item["tts"] for item in data if item["cur_unit"] == "KRW"), None)
+        nm = next((item["cur_nm"] for item in data if item["cur_unit"] == "EUR"), None)
+        ttb = next((item["ttb"] for item in data if item["cur_unit"] == "EUR"), None)
+        tts = next((item["tts"] for item in data if item["cur_unit"] == "EUR"), None)
         return f"국가: {nm}, TTB(전신환받을때): {ttb}, TTS(전신환보낼때): {tts}"
     else:
         return "환율 정보를 가져오는 데 실패했습니다."
@@ -32,7 +32,7 @@ def update_readme():
     readme_content = f"""
 # Weather API Status
 
-이 리포지토리는 OpenWeather API를 사용하여 서울의 날씨 정보를 자동으로 업데이트합니다.
+이 리포지토리는 환율 정보를 5분마다 자동으로 업데이트합니다.
 
 ## 현재 서울 날씨
 > {exchange_info}
